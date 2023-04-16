8a308bac8b3ea627197a5b8009aabf3ae23aae65
tomo <whowillcatchatastraw@gmail.com>
2023-04-16 14:42:00 +0900

    comment out each relay turms LOW

--- assembling_tests/pcb_test/pcb_test.ino
+++ assembling_tests/pcb_test/pcb_test.ino
@@ -56,37 +56,37 @@ void relay(){
       Serial.println("relay HIGH : CR1, Arduino pin No.31, FWD_L");
       digitalWrite(RightREV, HIGH); // 31
       delay(400);
-      digitalWrite(RightREV, LOW); // 31
+      // digitalWrite(1RightREV, LOW); // 31
     }
     else if (val == '2'){
       Serial.println("relay HIGH : CR2, Arduino pin No.33");  
       digitalWrite(RightFWD, HIGH); // 33
       delay(400);
-      digitalWrite(RightFWD, LOW); // 33
+      // digitalWrite(RightFWD, LOW); // 33
     }
     else if (val == '3'){
     Serial.println("relay HIGH : CR3, Arduino pin No.35");  
       digitalWrite(LeftStopMode, HIGH); // 35
       delay(400);
-      digitalWrite(LeftStopMode, LOW); // 35
+      // digitalWrite(LeftStopMode, LOW); // 35
     }
     else if (val == '4'){
     Serial.println("relay HIGH : CR4, Arduino pin No.37");  
       digitalWrite(Leftm0, HIGH); // 37
       delay(400);
-      digitalWrite(Leftm0, LOW); // 37
+      // digitalWrite(Leftm0, LOW); // 37
     }
     else if (val == '5'){
     Serial.println("relay HIGH : CR5, Arduino pin No39.");  
       digitalWrite(LeftAlarmReset, HIGH); // 39
       delay(400);
-      digitalWrite(LeftAlarmReset, LOW); // 39
+      // digitalWrite(LeftAlarmReset, LOW); // 39
     }
     else if (val == '6'){
     Serial.println("relay HIGH : CR6, Arduino pin No.41");  
       digitalWrite(LeftmbFree, HIGH); // 41
       delay(400);
-      digitalWrite(LeftmbFree, LOW); // 41
+      // digitalWrite(LeftmbFree, LOW); // 41
     }
 
     // 右側リレー個別確認
@@ -94,37 +94,37 @@ void relay(){
       Serial.println("relay HIGH : CR7, Arduino pin No.43");  
       digitalWrite(LeftFWD, HIGH); // 43
       delay(400);
-      digitalWrite(LeftFWD, LOW); // 43
+      // digitalWrite(LeftFWD, LOW); // 43
     }
     else if (val == '8'){
       Serial.println("relay HIGH : CR8, Arduino pin No.45");  
       digitalWrite(LeftREV, HIGH); // 45
       delay(400);
-      digitalWrite(LeftREV, LOW); // 45
+      // digitalWrite(LeftREV, LOW); // 45
     }
     else if (val == '9'){
       Serial.println("relay HIGH : CR9, Arduino pin No.47");  
       digitalWrite(RightStopMode, HIGH); // 47
       delay(400);
-      digitalWrite(RightStopMode, LOW); // 47
+      // digitalWrite(RightStopMode, LOW); // 47
     }
     else if (val == '10'){
       Serial.println("relay HIGH : CR10, Arduino pin No.49");  
       digitalWrite(Rightm0, HIGH); // 49
       delay(400);
-      digitalWrite(Rightm0, LOW); // 49
+      // digitalWrite(Rightm0, LOW); // 49
     }
     else if (val == '11'){
       Serial.println("relay HIGH : CR11, Arduino pin No.51");  
       digitalWrite(RightAlarmReset, HIGH); // 51
       delay(400);
-      digitalWrite(RightAlarmReset, LOW); // 51
+      // digitalWrite(RightAlarmReset, LOW); // 51
     }
     else if (val == '12'){
       Serial.println("relay HIGH : CR12, Arduino pin No.53");  
       digitalWrite(RightmbFree, HIGH); // 53
       delay(400);
-      digitalWrite(RightmbFree, LOW); // 53
+      // digitalWrite(RightmbFree, LOW); // 53
     }
 
     else if (val == 'l'){
