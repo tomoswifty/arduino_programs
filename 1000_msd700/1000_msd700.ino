871833e1fd2e0a4d8b1aad48450fcca663cef71b
tomoswifty <whowillcatchatastraw@gmail.com>
2023-04-19 19:41:52 +0900

    remodeling msd arduino code

--- 1000_msd700/1000_msd700.ino
+++ 1000_msd700/1000_msd700.ino
@@ -1,10 +1,19 @@
-/*
-2022-10-26
+/* 
+author:Tomoki Sugimoto
+created date:20230217
+purpose: To control the oriental motor from the proportional controler signal
+tested for MSD700 No.1005
 
+updates:
+2022-10-26 create file
+           change variable name 
+2023-02-15 Rewriting cylinders function
+           Rewriting motors function
+2023-02-16 add proportional controler function
+           add IK calcuration
+           Rewrite motor drive function
+           fix propo to velocity
 */
-
-
-
 
 #define RightmbFree 53
 #define RightAlarmReset 51
@@ -28,75 +37,251 @@
 #define LeftAlarmOutN 30
 #define LeftmmPin 2
 
-#define DumpFWD 50
-#define DumpREV 52
-#define LiftFWD 46
-#define LiftREV 48
+#define c1FWD 46
+#define c1REV 48
+#define c2FWD 50
+#define c2REV 52
 
 #define LiftFB A0
 #define DumpFB A1
 
+#define SwitcherOut 20
+
+#define soc1 22    // pcb 4 DO4
+#define soc2 24    // pcb 3 DO5
+#define RedLED 16  // pcb 6
 #define BatteryPin A2
-const int ch1 = 13;
-const int ch3 = 11;
-const int ch2 = 12;
-const int ch4 = 10;
-const int ch5 = 9;
-const int ch7 = 7;
-const int SwitcherIn = 8;
-const int SwitcherOut = 20;
 
-int BatteryV = 0, Type = 0, VR = 0, switcher = 0, gap;
-int RightX = 0, RightY = 0, LeftX, LeftXa, LeftXb, LeftY = 0;
-double DumpData = 0,LiftData = 0;
-int thresholdHigh = 1715, thresholdMid = 1510, thresholdLow = 1308;//プロポ閾値
+#define M_PI 3.14159265358979323846
 
+const int CH1 = 8;  // right horizontal = X
+const int CH2 = 9;  // left vertical = Y
+const int CH3 = 6;  // right vertical = Y
+const int CH4 = 7;  // left horizontal = X
+// const int CH5 = 10; // switch(B)
+const int CH6 = 10;  // SwitcherIn 
+const int CH7 = 12; // toggle "VR"
 
-/*for Serialprint*/
-long CurrentType;
-int speedX,speedY, Extend = 0, Retract = 0;
+int thresholdHigh = 1715, thresholdMid = 1510, thresholdLow = 1308; // プロポ閾値
+int rightX = 0; // right stick horizontal
+int rightY = 0; // right stick vartical
+int leftX  = 0; // left stick horizontal
+int leftY  = 0; // left stick vartical
+int VR = 0;
+int switcher = 0; // variable for ch6 switch
+
+int current_type;
+int type = 0;
+
 double batteryVo, batteryVoltage;
+int BatteryV = 0;
 
-void AttachmentModeChange(){
-    CurrentType = digitalRead(ch5);
-    switch (CurrentType){
-    case 0:
-        Type = 0;
-        break;
-    case 1:
-        Type = 1;
-        break;
-    }
+float motorRpm[2];
+float gLinear_x = 0.0;
+float gAngular_z = 0.0;
+
+int Extend = 0, Retract = 0;
+// double DumpData = 0,LiftData = 0;
+
+
+
+void proportionalControler(){
+  leftX  = pulseIn(CH2, HIGH);  // CH2の計測を開始
+  leftY  = pulseIn(CH4, HIGH);  // CH4の計測を開始
+  rightX = pulseIn(CH1, HIGH);  // CH1の計測を開始
+  rightY = pulseIn(CH3, HIGH);  // CH3の計測を開始
+
+  // twist value to velosity
+  // 1308.0  1715.0
+  // for linear
+  if(leftY >= thresholdMid){ // plus
+    gLinear_x  = map(leftY, 1105, thresholdMid, 0, 462) / 1000; 
+  }
+  else if(leftY <= thresholdMid){ // minus
+    gLinear_x  = - map(leftY, thresholdMid, 1919, 0, 462) / 1000; 
+  }
+  // for angular
+  if(leftX >= thresholdMid){ // plus
+    gAngular_z = map(leftX, 1105, 1919, -78, 78) / 100; 
+  }
+  else if(leftX <= thresholdMid){ // minus
+    gAngular_z = - map(leftX, thresholdMid, 1919, 0, 78) / 100; 
+  }
+  
+  // monitoring for check variables
+  Serial.print(leftY);
+  Serial.print(",");
+  Serial.print(leftX);
+  Serial.print(",");
+  Serial.print(gLinear_x);
+  Serial.print(",");
+  Serial.println(gAngular_z);
+}
+
+void doHarness(){
+  if(digitalRead(soc1) == HIGH && digitalRead(soc2) == HIGH){
+    digitalWrite(RedLED,HIGH);
+    // Serial.println("battery is LOW. 25% cherged");
+  }
+  else{
+    digitalWrite(RedLED,LOW);
+    // Serial.println("battery is cherged");
+  }
 }
 
 void batteryData(){
-    BatteryV = analogRead(BatteryPin);
-      batteryVo = ((BatteryV+0.5)*5.00)/1024;
-      batteryVoltage = batteryVo*30.00/5.00;
-    //Serial.print(" Battery Voltage : ");  
-    Serial.print(BatteryV); 
-    Serial.print(","); Serial.print(batteryVo);
-    Serial.print(","); Serial.println(batteryVoltage);
+  BatteryV = analogRead(BatteryPin);
+  batteryVo = ((BatteryV + 0.5) * 5.00) / 1024;
+  batteryVoltage = batteryVo * 30.00 / 5.00;
+  //Serial.print(" Battery Voltage : ");  
+  // Serial.print(BatteryV); 
+  // Serial.print(","); Serial.print(batteryVo);
+  // Serial.print(","); Serial.println(batteryVoltage);
+}
+
+// void attachmentModeChange(){
+//   /* change direction 
+//   blade type : 0 
+//   bucket type : 1 */
+//   current_type = digitalRead(CH5);
+//   switch (current_type){
+//   case 0:
+//     type = 0;
+//     break;
+//   case 1:
+//     type = 1;
+//     break;
+//   default:
+//     type = 0;
+//   }
+// }
+
+
+double velToRpm(double v){
+  // convert velocity to rpm 
+  const double REDUCTION_RATIO = 100.0;  // 減速比 reduction ratio
+  const double WHEEL_RADIUS = 0.1105;  // 車輪半径 radius of gyration [m]
+  double n = 30 / M_PI * REDUCTION_RATIO / WHEEL_RADIUS * v;
+  
+  return n;
+}
+
+void ikVelToRpm(double v, double w){
+  // calculate linear velocity and angur velocity to left and right velocity
+  double D = 0.600;     // 車輪間距離 distance between the wheels [m]
+  double v_r = v + D * w;
+  double v_l = v - D * w;
+
+  double left_rpm = velToRpm(v_l);
+  double right_rpm = velToRpm(v_r);
+  
+  motorRpm[0] = left_rpm;
+  motorRpm[1] = right_rpm;
+}
+
+
+void cylinderMove(int pulse, int FWD, int REV){
+  if(pulse >= thresholdHigh){
+    digitalWrite(REV, HIGH);
+  }
+  else if (pulse <= thresholdLow){
+    digitalWrite(FWD, HIGH);
+  }
+  else{
+    digitalWrite(FWD, LOW);
+    digitalWrite(REV, LOW);
+  }
+}
+
+void cylinderStop(int c1f, int c1r, int c2f, int c2r){
+  digitalWrite(c1f, LOW);
+  digitalWrite(c1r, LOW);
+  digitalWrite(c2f, LOW);
+  digitalWrite(c2r, LOW);
+  
+  // Sample for additional cylinder
+  // ", int c3f, int c3r"
+  // digitalWrite(c3f, LOW);
+  // digitalWrite(c3r, LOW);
+}
+
+void motorStop(){
+  analogWrite(LeftmmPin, 0);
+  analogWrite(RightmmPin, 0);
+
+  // HIGH:ブレーキ解放 LOW:ブレーキ保持
+  digitalWrite(LeftmbFree, LOW);
+  digitalWrite(RightmbFree, LOW);
+
+  // STOP表示
+  Serial.println("Motor Stop ");
+}
+
+void motorDirection(long left_rpm_in, long right_rpm_in){
+  long left_rpm;
+  long right_rpm;
+  
+  if(left_rpm_in > 0){
+    left_rpm = left_rpm_in;
+    digitalWrite(LeftFWD, HIGH);
+  }
+  else if (left_rpm_in > 0){
+    right_rpm = left_rpm_in;
+    digitalWrite(RightREV, HIGH);
+  }
+  else{
+    digitalWrite(LeftFWD, LOW);
+    digitalWrite(LeftREV, LOW);
+  }
+
+  if(right_rpm_in > 0){
+    right_rpm = right_rpm_in;
+    digitalWrite(RightFWD, HIGH);
+  }
+  else if (right_rpm_in < 0){
+    left_rpm = right_rpm_in;
+    digitalWrite(LeftREV, HIGH);
+  }
+  else{
+    digitalWrite(RightFWD, LOW);
+    digitalWrite(RightREV, LOW);
+  }
+}
+
+void motorDrive(int left_rpm, int right_rpm){
+  // twist
+  int left_byte = map(left_rpm, 0, 4000, 0,255);
+  int right_byte = map(right_rpm, 0, 4000, 0,255);
+
+  motorDirection(left_byte, right_byte);
+  
+  if(left_byte && right_byte != 0.0){
+    digitalWrite(LeftmmPin, left_byte);
+    digitalWrite(RightmmPin, right_byte);
+    Serial.print(left_byte);
+    Serial.print(",");
+    Serial.println(right_byte);
+  }
+  else{
+    motorStop();
+  }
 }
 
 void printall(){
   /*battery output values for IoT sysytem
   IoTシステム用バッテリー出力値*/
-    //Serial.print(" Battery Voltage : ");  
+    Serial.print(" Battery Voltage : ");  
     Serial.print(BatteryV); 
     Serial.print(","); Serial.print(batteryVo);
     Serial.print(","); Serial.println(batteryVoltage);
      
   /*Value of the controller's switch for type switching
   タイプ切り替えのためのコントローラのスイッチの値*/
-    Serial.print(CurrentType);
+    Serial.print(current_type);
 
-  /*Motor speed output from MotorDrive function
+  /*Motor speed output from motorDrive function
   モータの速度の表示*/
-    Serial.print("speed X => ");
-    Serial.println(speedX);
-    Serial.print("speed Y => ");
-    Serial.println(speedY);
+
 
   /*Value of the attachment arms from Lift function
   バケット/ブレードのシリンダの値*/
@@ -106,293 +291,110 @@ void printall(){
     Serial.println(Retract);
 }
 
-void Lift(int pulse){   
-    bool dirLift = false;
-    bool canLift = false;
-    
-    if (pulse >= thresholdHigh) {
-      Retract = 1;
-    } else if (pulse <= thresholdLow) { 
-      Extend = 1;
-    }
-  
-    if (Extend == 1) {
-      dirLift = true;
-      canLift = true;
-    } else if (Retract == 1) {
-      dirLift = false;
-      canLift = true;
-    } else {
-      canLift = false;
-    }
-    digitalWrite(LiftFWD, dirLift & canLift);
-    digitalWrite(LiftREV, (!dirLift) & canLift);
-}
-
-void Dump(int pulse){
-  bool dirDump = false;
-  bool canDump = false;
-  
-  if (pulse >= thresholdHigh) {
-    Extend = 1;
-  } else if (pulse <= thresholdLow) { 
-    Retract = 1;
-  }
-
-  if (Extend == 1) {
-    dirDump = true;
-    canDump = true;
-  } else if (Retract == 1) {
-    dirDump = false;
-    canDump = true;
-  } else {
-    canDump = false;
-  }
-
-  if (Type == 0) {
-    digitalWrite(DumpFWD, dirDump & canDump);
-    digitalWrite(DumpREV, (!dirDump) & canDump);
-  } else {
-    digitalWrite(DumpFWD, (!dirDump) & canDump);
-    digitalWrite(DumpREV, dirDump & canDump);
-  }
-}
-
-void MotorDrive(int LeftX, int LeftY){
-    //移動方向フラグ
-    int Right = 0;
-    int Left = 0;
-    int Forward = 0;
-    int Reverse = 0;
-  
-    //モーター回転方向フラグ
-    bool dirR = false;
-    bool dirL = false;
-    bool canMove = false;
-  
-    // 移動方向決定
-    if (LeftX >= thresholdHigh) {
-      Right = 1;
-    } else if (LeftX <= thresholdLow) {
-      Left = 1;
-    }
-    if (LeftY >= thresholdHigh) {
-      Forward = 1;
-    } else if (LeftY <= thresholdLow) {
-      Reverse = 1;
-    }
-  
-    //前後左右フラグ反転
-    if(Type == 0){
-      int temp = Forward;
-      Forward = Reverse;
-      Reverse = temp;
-      temp = Right;
-      Right = Left;
-      Left = temp;
-    }
-  
-    //モーター動作決定
-    int diffX = abs(LeftX-thresholdMid);
-    int diffY = abs(LeftY-thresholdMid);
-    int speedX = map(diffX, 0, 410, 0, 255);
-    int speedY = map(diffY, 0, 410, 0, 255);
-
-    if (Forward == 1) {
-      canMove = true;
-      dirR = dirL = false;
-      if (Right == 1) {
-        //右前進
-        analogWrite(LeftmmPin, speedY);
-        analogWrite(RightmmPin, 0);
-      } else if (Left == 1) {
-        //左前進
-        analogWrite(LeftmmPin, 0);
-        analogWrite(RightmmPin, speedY);
-      } else {
-        //前進
-        analogWrite(LeftmmPin, speedY);
-        analogWrite(RightmmPin, speedY);
-      }
-    } else if (Reverse == 1) {
-      canMove = true;
-      dirR = dirL = true;
-      if (Right == 1) {
-        //右後進
-        analogWrite(LeftmmPin, speedY);
-        analogWrite(RightmmPin, 0);
-      } else if (Left == 1) {
-        //左後進
-        analogWrite(LeftmmPin, 0);
-        analogWrite(RightmmPin, speedY);
-      } else {
-        //後進
-        analogWrite(LeftmmPin, speedY);
-        analogWrite(RightmmPin, speedY);
-      }
-    } else {
-      if (Right == 1) {
-        //右旋回
-        canMove = true;
-        if (Type == 0) {
-          dirR = true;
-          dirL = false;
-        } else {
-          dirR = false;
-          dirL = true;  
-        }
-        analogWrite(LeftmmPin, speedX);
-        analogWrite(RightmmPin, speedX);
-      } else if (Left == 1) {
-        //左旋回
-        canMove = true;
-        if (Type == 0) {
-          dirR = false;
-          dirL = true;
-        } else {
-          dirR = true;
-          dirL = false;  
-        }
-        analogWrite(LeftmmPin, speedX);
-        analogWrite(RightmmPin, speedX);
-      } else {
-        //停止
-        canMove = false;
-        analogWrite(LeftmmPin, 0);
-        analogWrite(RightmmPin, 0);
-      }
-    }
-    digitalWrite(RightFWD, dirR & canMove);
-    digitalWrite(RightREV, (!dirR) & canMove);
-    digitalWrite(LeftFWD, dirL & canMove);
-    digitalWrite(LeftREV, (!dirL) & canMove);
-}
-
-void StopMachine(){
-  analogWrite(LeftmmPin, 0);//モーター速度0
-  analogWrite(RightmmPin, 0);
-  //前進後進指示なし
-  digitalWrite(LiftFWD, LOW);
-  digitalWrite(LiftREV, LOW);
-  digitalWrite(DumpFWD, LOW);
-  digitalWrite(DumpREV, LOW);
-
-  //HIGH:ブレーキ解放 LOW:ブレーキ保持
-  digitalWrite(LeftmbFree, LOW);
-  digitalWrite(RightmbFree, LOW);
-
-  // STOP表示
-  Serial.println(" Stop ");
-}
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
+  //右モータードライバーアウトプット
+  pinMode(RightFWD, OUTPUT);
+  pinMode(RightREV, OUTPUT);
+  pinMode(RightStopMode, OUTPUT);
+  pinMode(Rightm0, OUTPUT);
+  pinMode(RightAlarmReset, OUTPUT);
+  pinMode(RightmbFree, OUTPUT);
+  //右モータードライバーインプット
+  pinMode(RightAlarmOut, INPUT);
+  pinMode(RightwngN, INPUT);
+  pinMode(RightSpeedOut, INPUT);
 
-    //モータードライバー速度設定
-    pinMode(LeftmmPin, OUTPUT);
-    pinMode(RightmmPin, OUTPUT);
+  //左モータードライバーアウトプット
+  pinMode(LeftFWD, OUTPUT);
+  pinMode(LeftREV, OUTPUT);
+  pinMode(LeftStopMode, OUTPUT);
+  pinMode(Leftm0, OUTPUT);
+  pinMode(LeftAlarmReset, OUTPUT);
+  pinMode(LeftmbFree, OUTPUT);
+  //左モータードライバーインプット
+  pinMode(LeftAlarmOutN, INPUT);
+  pinMode(LeftwngN, INPUT);
+  pinMode(LeftSpeedOut, INPUT);
+
+  // HIGH:速度指定を外部入力 LOW:内部速度設定器
+  digitalWrite(Rightm0, HIGH);
+  digitalWrite(Leftm0, HIGH);
+  //モータードライバー速度設定
+  pinMode(LeftmmPin, OUTPUT);
+  pinMode(RightmmPin, OUTPUT);
+  // HIGH:時間設定 LOW:瞬時停止
+  digitalWrite(RightStopMode, LOW);
+  digitalWrite(LeftStopMode, LOW);
+  // HIGH:ブレーキ解放 LOW:ブレーキ保持
+  digitalWrite(RightmbFree, HIGH);
+  digitalWrite(LeftmbFree, HIGH);
+
+
+  // シリンダーモータードライバー
+  pinMode(c1FWD, OUTPUT);
+  pinMode(c1REV, OUTPUT);
+  pinMode(c2FWD, OUTPUT);
+  pinMode(c2REV, OUTPUT);
+  // アームフィードバック
+  pinMode(LiftFB, INPUT);
+  pinMode(DumpFB, INPUT);
+
+  // レシーバー 
+  pinMode(CH1, INPUT);
+  pinMode(CH3, INPUT);
+  pinMode(CH2, INPUT);
+  pinMode(CH4, INPUT);
+  // pinMode(CH5, INPUT);
+  pinMode(CH7, INPUT);
+  pinMode(CH6, INPUT);
+  pinMode(SwitcherOut, OUTPUT); // 動力リレー用リレー
+
+  // Battery Pin
+  pinMode(BatteryPin, INPUT);
+  pinMode(RedLED, OUTPUT);
   
-    //右モータードライバーアウトプット
-    pinMode(RightFWD, OUTPUT);
-    pinMode(RightREV, OUTPUT);
-    pinMode(RightStopMode, OUTPUT);
-    pinMode(Rightm0, OUTPUT);
-    pinMode(RightAlarmReset, OUTPUT);
-    pinMode(RightmbFree, OUTPUT);
-  
-    //右モータードライバーインプット
-    pinMode(RightAlarmOut, INPUT);
-    pinMode(RightwngN, INPUT);
-    pinMode(RightSpeedOut, INPUT);
-  
-    //左モータードライバーアウトプット
-    pinMode(LeftFWD, OUTPUT);
-    pinMode(LeftREV, OUTPUT);
-    pinMode(LeftStopMode; OUTPUT);
-    pinMode(Leftm0, OUTPUT);
-    pinMode(LeftAlarmReset, OUTPUT);
-    pinMode(LeftmbFree, OUTPUT);
-  
-    //左モータードライバーインプット
-    pinMode(LeftAlarmOutN, INPUT);
-    pinMode(LeftwngN, INPUT);
-    pinMode(LeftSpeedOut, INPUT);
-  
-    //シリンダーモータードライバー
-    pinMode(LiftFWD, OUTPUT);
-    pinMode(LiftREV, OUTPUT);
-    pinMode(DumpFWD, OUTPUT);
-    pinMode(DumpREV, OUTPUT);
-  
-    //レシーバー
-    pinMode(ch1, INPUT);
-    pinMode(ch3, INPUT);
-    pinMode(ch2, INPUT);
-    pinMode(ch4, INPUT);
-    pinMode(ch5, INPUT);
-    pinMode(ch7, INPUT);
-    pinMode(SwitcherIn, INPUT);
-    pinMode(SwitcherOut, OUTPUT);    //動力リレー用リレー
-  
-    //アームフィードバック
-    pinMode(LiftFB, INPUT);
-    pinMode(DumpFB, INPUT);
-  
-    // HIGH:時間設定 LOW:瞬時停止
-    digitalWrite(RightStopMode, LOW);
-    digitalWrite(LeftStopMode, LOW);
-    
-    // HIGH:速度指定を外部入力 LOW:内部速度設定器
-    digitalWrite(Rightm0, HIGH);
-    digitalWrite(Leftm0, HIGH);
-    
-    //HIGH:ブレーキ解放 LOW:ブレーキ保持
-    digitalWrite(RightmbFree, HIGH);
-    digitalWrite(LeftmbFree, HIGH);
-  
-    //Battery Pin
-    pinMode(BatteryPin, INPUT);
 }
 
 void loop() {
-    AttachmentModeChange();
-    // batteryData();
-    switcher = pulseIn(SwitcherIn, HIGH);
+  switcher = pulseIn(CH6, HIGH);
+  proportionalControler();
+  // attachmentModeChange();
+  batteryData();
+  doHarness();
 
-    if (switcher > 1550) {
-        digitalWrite(SwitcherOut, LOW);
-        VR = pulseIn(ch7, HIGH);
+  if (switcher > 1550) {
+      digitalWrite(SwitcherOut, LOW);
+      VR = pulseIn(CH7, HIGH);
 
     if (VR > 1700) {
-      LeftXb = LeftXa;
-      RightX = pulseIn(ch1, HIGH); // ch1の計測を開始
-      RightY = pulseIn(ch3, HIGH); // ch3の計測を開始
-      LeftX = pulseIn(ch2, HIGH);  // ch2の計測を開始
-      LeftY = pulseIn(ch4, HIGH);  // ch4の計測を開始
-      LeftXa = LeftX;
+      // ----------------test---------------- 
+      // gLinear_x, gAngular_z
+      // float gLinear_x = 0.011; // 0.4628;   // [m/s]
+      // float gAngular_z = 0.0;     // [rad/s]
+      // ------------------------------------
+      ikVelToRpm(gLinear_x, gAngular_z);
 
-      gap = abs(LeftXb - LeftXa);
-      if (LeftX < 1000 || LeftX > 2000) {
-        LeftX = LeftXb;
-        LeftXa = LeftXb;
-      } else {
-        LeftX = LeftX;
-        LeftXa = LeftXa;
+      motorDrive(motorRpm[0],motorRpm[1]);
+      
+      Serial.print("left motor value: ");
+      Serial.print(motorRpm[0]);
+      Serial.print(", right motor value: ");
+      Serial.println(motorRpm[1]);
+
+      cylinderMove(rightY, c1FWD, c1REV); // cylinder 1
+      cylinderMove(rightX, c2REV, c2FWD); // cylinder 2
       }
-
-      //アーム動作
-      Lift(RightY);
-      Dump(RightX);
-
-      // モーター動作
-      MotorDrive(LeftX, LeftY);
-    } else {
-      StopMachine();
-    }
-    } else {
-      digitalWrite(SwitcherOut, HIGH);
-      StopMachine();
-    }
-
-    printall();
+      else {
+        motorStop();
+        cylinderStop(c1FWD, c1REV, c2FWD, c2REV);
+      }
+  }
+  else {
+    digitalWrite(SwitcherOut, HIGH);
+    motorStop();
+    cylinderStop(c1FWD, c1REV, c2FWD, c2REV);
+  }
+  // printall();
 }
