1f304d7c3877848e82b496bc11e8a0991dc742fa
TatsuyaKoyanagi <131863139+TatsuyaKoyanagi@users.noreply.github.com>
2023-04-26 18:14:32 +0900

    エンコーダーファイルアップロード

---
+++ test/motor_test/MSD700_pulseIn/MSD700_pulseIn.ino
@@ -0,0 +1,26 @@
+const int PIN = 21;
+void setup() {
+  pinMode(PIN, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  unsigned long  interval_HIGH = 0;
+  unsigned long  interval_LOW = 0;
+  unsigned long  interval = 0;
+  unsigned long  rpm = 0;
+  interval = 2 * pulseIn(PIN, LOW, 300000);
+  //interval_HIGH = pulseIn(PIN,HIGH);
+  //interval_LOW = pulseIn(PIN,LOW);
+  //interval=interval_HIGH+interval_LOW;
+
+  rpm = 4000000 / interval; //1パルス4秒
+
+  if (rpm <= 5000) {
+    Serial.print(rpm);
+    Serial.print(",");
+    Serial.print(00);
+    Serial.print("\n");
+  }
+  //  Serial.println(rpm);
+}
